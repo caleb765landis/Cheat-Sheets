35bf8afb291ade0895ebfe57769cb52d0d31a0fb
caleb765landis <caleb765landis@gmail.com>
2023-09-11 10:32:28 -0600

    Updated Classes

--- Python/Classes.py
+++ Python/Classes.py
@@ -1,9 +1,10 @@
 class MyClass:
     # Constructor
-    def __init__(self, nums):
+    def __init__(self, nums, name="Anonymous"):
         # Create member variables
         self.nums = nums
         self.size = len(nums)
+        self.name = name
 
     # self key word required as param
     def getLength(self):
@@ -11,8 +12,19 @@ class MyClass:
 
     def getDoubleLength(self):
         return 2 * self.getLength()
+    
+    def getName(self):
+        return self.__name
+
+    def setName(self, name):
+        self.__name = name
+
+    name = property(getName, setName)
 
 
 myObj = MyClass([1, 2, 3])
 print(myObj.getLength())
 print(myObj.getDoubleLength())
+print(myObj.name)
+myObj.name = "Guido"
+print(myObj.name)
